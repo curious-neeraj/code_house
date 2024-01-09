644fc1f1e6f82aaa5dd1b96f79b301c49776e8f3
Neeraj Jain <145219538+curious-neeraj@users.noreply.github.com>
2024-01-09 23:58:37 +0530

    Commit Solution - trapping_rain_water.cpp

---
+++ dsa/two_pointers/trapping_rain_water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int water = 0;
+        int len = height.size();
+        
+        int left = 0, right = len-1;
+        int leftMax = height[0], rightMax = height[len-1];
+        
+        while(left < right)
+        {
+            if(height[left] < height[right])
+            {
+                leftMax = max(leftMax, height[left]);           
+                water += leftMax - height[left];
+                left++;
+            }
+            else
+            {
+                rightMax = max(rightMax, height[right]); 
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        
+        return water;
+    }
+};
