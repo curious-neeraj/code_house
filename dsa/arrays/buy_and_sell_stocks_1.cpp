0764f28264e0caa32314ad0fd33d5f6e21e5bd49
Neeraj Jain <145219538+curious-neeraj@users.noreply.github.com>
2024-01-10 19:41:50 +0530

    Commit Solution - buy_and_sell_stocks_1.cpp

---
+++ dsa/arrays/buy_and_sell_stocks_1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int profit = 0;
+        int len = prices.size();
+        if(len <= 1)
+            return 0;
+
+        int buy = prices[0];
+        for(int i = 1; i < len; i++)
+        {
+            buy = min(buy, prices[i]);
+            profit = max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+};
