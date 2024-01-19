63ce52d3e6b6038b79b5da0bbc38cc493617cd00
Neeraj Jain <145219538+curious-neeraj@users.noreply.github.com>
2024-01-19 21:08:11 +0530

    Commit Solution - minimum_window_substring.cpp

---
+++ dsa/sliding_window/minimum_window_substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+        vector<int> map(128,0);
+        for (char c : t) 
+            map[c]++;
+
+        int t_size = t.size(), s_size = s.size(), begin = 0, end = 0;
+        int window = INT_MAX, head = 0;
+        while (end < s_size)
+        {
+            if (map[s[end++]]-- > 0)
+                t_size--;
+            
+            while (t_size == 0) 
+            {
+                if (end - begin < window) 
+                {
+                    head = begin;
+                    window = end - head;
+                }
+                if (map[s[begin++]]++ == 0)
+                    t_size++;
+            }
+        }
+        return window == INT_MAX ? "" : s.substr(head, window);
+    }
+};
